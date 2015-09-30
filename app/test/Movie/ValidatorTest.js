13cc41e118c05378ffd77396a505400e1b7ae278
kisPocok <david.schneidhoffer@gmail.com>
2015-10-01 00:34:05 +0200

    Fix tests

--- app/test/Movie/ValidatorTest.js
+++ app/test/Movie/ValidatorTest.js
@@ -10,11 +10,9 @@ describe("Movie/Helper", function() {
             var actual = Helper.isDir('.');
             expect(true).to.equal(actual);
         });
-
         it("invalidDirectoryShouldThrowEnoentError", function() {
             expect(Helper.isDir.bind(Helper, './ValidatorTest.js')).to.throw(/ENOENT/); // No such file or dir
         });
-
         it("unreadableDirectoryShouldThrowEaccessError", function() {
             expect(Helper.isDir.bind(Helper, './test/Movie/UnreadableDir/')).to.throw(/EACCES/); // permission denied
         });
@@ -39,24 +37,28 @@ describe("Movie/Helper", function() {
     });
 
     describe("removeFileExtension", function() {
-        it("removeFileExtensionShouldOk", function() {
+        it("removeFileExtensionShouldPass", function() {
             var expected = 'Walking.Dead.s06e01';
             var actual = Helper.removeFileExtension(expected + '.mkv');
             expect(expected).to.equal(actual);
+        });
+        it("withoutExtensionShouldPassOriginalValue", function() {
+            var data = 'almafa';
+            var actual = Helper.removeFileExtension(data);
+            expect(data).to.equal(actual);
         });
     });
 
     describe("getFileExtension", function() {
         it("rightFileNameShouldPass", function() {
             var data = 'Walking.Dead.s06e01.mkv';
-            var actual = Helper.removeFileExtension(data);
-            expect('Walking.Dead.s06e01').to.equal(actual);
-        });
-
-        it("emptyFileNameShouldThrowError", function() {
-            var actual = Helper.removeFileExtension();
-            expect(Helper.removeFileExtension.bind(Helper)).to.throw(/Cannot read property/); // permission denied
+            var actual = Helper.getFileExtension(data);
             expect('mkv').to.equal(actual);
+        });
+        it("withoutExtensionShouldPassOriginalValue", function() {
+            var data = 'almafa';
+            var actual = Helper.getFileExtension(data);
+            expect(data).to.equal(actual);
         });
     });
 });
