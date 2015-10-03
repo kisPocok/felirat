d3af43634a06b556aa13d7842a6405cc97859fd3
kisPocok <david.schneidhoffer@gmail.com>
2015-10-04 00:49:12 +0200

    Directory tests fixed

--- app/test/Movie/ValidatorTest.js
+++ app/test/Movie/ValidatorTest.js
@@ -1,8 +1,25 @@
 'use strict';
 
+var fs = require('fs');
 var expect = require("chai").expect;
 var should = require("chai").should();
 var Helper = require('../../resources/Movie/Helper');
+
+var createDirectoryIfNotExists = function (path, chmod) {
+    try {
+        fs.lstatSync(path);
+    } catch (e) {
+        fs.mkdirSync(path);
+    }
+    fs.chmodSync(path, chmod);
+};
+var createUnreadableDirectory = function (path) {
+    return createDirectoryIfNotExists(path, '312');
+};
+var createUnwritableDirDirectory = function (path) {
+    return createDirectoryIfNotExists(path, '556');
+};
+
 
 describe("Movie/Helper", function() {
     describe("isDir", function() {
@@ -14,7 +31,9 @@ describe("Movie/Helper", function() {
             expect(Helper.isDir.bind(Helper, './ValidatorTest.js')).to.throw(/ENOENT/); // No such file or dir
         });
         it("unreadableDirectoryShouldThrowEaccessError", function() {
-            expect(Helper.isDir.bind(Helper, './test/Movie/UnreadableDir/')).to.throw(/EACCES/); // permission denied
+            var path = './test/Movie/Test_UnreadableDir/';
+            createUnreadableDirectory(path);
+            expect(Helper.isDir.bind(Helper, path)).to.throw(/EACCES/); // permission denied
         });
     });
 
@@ -23,7 +42,9 @@ describe("Movie/Helper", function() {
             expect(Helper.isWritable.bind(Helper, './test/Movie/')).to.not.throw(/E/);
         });
         it("unwritableDirectoryShouldThrowEaccessError", function() {
-            expect(Helper.isWritable.bind(Helper, './test/Movie/UnwritableDir/')).to.throw(/EACCES/); // permission denied
+            var path = './test/Movie/Test_UnwritableDir/';
+            createUnwritableDirDirectory(path);
+            expect(Helper.isWritable.bind(Helper, path)).to.throw(/EACCES/); // permission denied
         });
     });
 
@@ -32,7 +53,9 @@ describe("Movie/Helper", function() {
             expect(Helper.isReadable.bind(Helper, './test/Movie/')).to.not.throw(/E/);
         });
         it("readableDirectoryShouldntThrowEaccessError", function() {
-            expect(Helper.isReadable.bind(Helper, './test/Movie/UnreadableDir/')).to.throw(/EACCES/); // permission denied
+            var path = './test/Movie/Test_UnreadableDir/';
+            createUnreadableDirectory(path);
+            expect(Helper.isReadable.bind(Helper, path)).to.throw(/EACCES/); // permission denied
         });
     });
 
